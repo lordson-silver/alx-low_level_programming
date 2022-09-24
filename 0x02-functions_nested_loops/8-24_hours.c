a2bf48ae91f826744ff0d9df11d8bb4c10bd8816
lordson-silver <lordsonochuko5@gmail.com>
2022-09-24 06:48:49 -0700

    Function that prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,7 +3,8 @@
 
 /**
  * jack_bauer - function
- * Description: function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * Description: function that prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
  * Return: void
  */
 
