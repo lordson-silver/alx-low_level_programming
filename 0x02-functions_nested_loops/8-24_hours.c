8fada83ed55a4bf3cbdd5d3a7aa21e744bc5be95
lordson-silver <lordsonochuko5@gmail.com>
2022-09-24 06:44:38 -0700

    Function that prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,21 +16,21 @@ void jack_bauer(void)
 {
 	int i, j, k, l, m;
 
-	i = 0;
-	while (i < 3)
+	i = '0';
+	while (i < '3')
 	{
-		j = 0;
-		m = 9;
-		if (i == 2)
-			m = 3;
+		j = '0';
+		m = '9';
+		if (i == '2')
+			m = '3';
 
 		while (j <= m)
 		{
-			k = 0;
-			while (k < 6)
+			k = '0';
+			while (k < '6')
 			{
-				l = 0;
-				while (l <= 9)
+				l = '0';
+				while (l <= '9')
 				{
 					_putchar(i);
 					_putchar(j);
@@ -40,13 +40,13 @@ void jack_bauer(void)
 					_putchar('\n');
 					l++;
 				}
-				l = 0;
+				l = '0';
 				k++;
 			}
-			k = 0;
+			k = '0';
 			j++;
 		}
-		j = 0;
+		j = '0';
 		i++;
 	}
 }
