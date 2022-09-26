ef6b4c8bc2f1f96350b7856349cf5f316022ff8e
lordson-silver <lordsonochuko5@gmail.com>
2022-09-26 07:09:50 -0700

    Program tgat prints fib sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - c main funcyion
+ * Return: always 
+ */
+void _fibgen(int n);
+int main(void)
+{
+	_fibgen(50);
+	return (0);
+}
+
+/**
+ * _fibgen - fibonacci generator
+ * @n: int param
+ * Return: void
+ */
+void _fibgen(int n)
+{
+	int a, b;
+
+	a = 0, b = 1;
+
+	while (a < n)
+	{
+		printf("%d, ", b += b);
+		a++;
+	}
+	printf("\n");
+}
