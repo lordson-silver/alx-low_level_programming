2ab9924b5fb59a4e9f71e7d0316ea587c67c0f06
lordson-silver <lordsonochuko5@gmail.com>
2022-09-27 11:18:03 -0700

    Function that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string function
+ * @dest: destination pointer
+ * @src: source pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
