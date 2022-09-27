a3d59599bf9b97830e62f16aeb8fcd34b006e270
lordson-silver <lordsonochuko5@gmail.com>
2022-09-27 12:10:48 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcat - append src to dest
+ * @dest: pointer param
+ * @src: pointer param
+ * Return: strcat
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
