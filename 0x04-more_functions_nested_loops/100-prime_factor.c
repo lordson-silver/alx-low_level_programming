5468916f2febbf1b2d32072ce1f0b72355b268e0
lordson-silver <lordsonochuko5@gmail.com>
2022-09-27 14:56:12 +0100

    Function that prints the largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always 0
+ */
+void prime(void);
+int main(void)
+{
+	prime();
+
+	return (0);
+}
+
+/**
+ * prime - prints the largest prime factor
+ */
+void prime(void)
+{
+	long int i, p, pf = 612852475143;
+
+	i = 2;
+	while (i <= 612852475143 && pf > i)
+	{
+		p = 1;
+		if ((pf % p) == 0)
+			pf = pf / p;
+		else
+			i++;
+	}
+	printf("%ld\n", p);
+}
