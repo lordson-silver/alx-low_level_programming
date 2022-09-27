7825a62ee064bb2bce708e464877172fea056f71
lordson-silver <lordsonochuko5@gmail.com>
2022-09-27 11:51:56 -0700

    Program that generates random valid passwords for the program 101-crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i]);
+		printf("%d", pass[i]);
+		if ((2772 - sum) > 78)
+		{
+			n = 2772 - sum;
+			sum += n;
+			printf("%d", n);
+			break;
+		}
+	}
+
+	return (0);
+}
