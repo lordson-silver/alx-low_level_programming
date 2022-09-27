cbb63f50d93a70f463ee7ea7759a9e28e1bbf208
lordson-silver <lordsonochuko5@gmail.com>
2022-09-27 01:44:15 +0100

    Function that prints 1 - 14, 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 1 - 1
+ * @i: int counter
+ * @j: int counter
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j + '0');
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}	
+	_putchar('\n');
+}
