8256e81504af0adf9cb09b08af0673ed097a1671
lordson-silver <lordsonochuko5@gmail.com>
2022-09-27 13:46:11 +0100

    Function that prints the fizzbuzz algorithm

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: always 
+ */
+void fizz(void);
+int main(void)
+{
+	fizz();
+	return (0);
+}
+
+/**
+ * fizz - function that prints the fizzbuzz
+ * algorithm lol
+ * Return: void
+ */
+void fizz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf(" FizzBuzz");
+		else if (i % 3 == 0)
+			printf(" Fizz");
+		else if (i % 5 == 0)
+			printf(" Buzz");
+		else if (i == 1)
+			printf("%d", i);
+		else
+			printf(" %d", i);
+	}
+}
