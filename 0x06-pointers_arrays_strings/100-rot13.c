3080b63e2ed94150debe7979d011435e8ca73587
lordson-silver <lordsonochuko5@gmail.com>
2022-09-27 19:18:21 -0700

    Function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string to encode
+ * Return: resulting string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; a[j]; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = a[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *s);
 char *leet(char *s);
+char *rot13(char *s);
