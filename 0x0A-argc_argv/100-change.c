d50cb2c4365e35db07ae834729ef435710d02c7a
lordson-silver <lordsonochuko5@gmail.com>
2022-10-21 04:25:50 +0100

    --no-edit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,39 +2,9 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change for an amount of
- * money.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success, 1 - fail.
- */
-int change(int cents);
-int main(int argc, char *argv[])
-{
-	int cents, coins;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	coins = 0;
-	change(cents, &coins);
-	printf("%d\n", coins);
-	return (0);
-}
-
-/**
  * change - calculates the minimum number of coins to make change for an amount
  * of money.
  * @cents: amount of money.
- * @coins: number of coins.
  * Return: coins.
  */
 int change(int cents)
@@ -67,4 +37,32 @@ int change(int cents)
 		coins++;
 	}
 	return (coins);
+}
+
+/**
+ * main - prints the minimum number of coins to make change for an amount of
+ * money.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success, 1 - fail.
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	coins = 0;
+	change(cents, &coins);
+	printf("%d\n", coins);
+	return (0);
 }
