24c46395682bd4601794700471238d37f3b1a276
lordson-silver <lordsonochuko5@gmail.com>
2022-10-21 03:46:53 +0100

    Preogram that prints the product of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the product of two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, product;
+
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	product = i * j;
+	printf("%d\n", product);
+	return (0);
+}
